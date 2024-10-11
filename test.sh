3958d55d78cc52351cb4cbc499d4015fd85f63d5
IliaTere <ilia.tereshenko@math.msu.ru>
2024-10-11 14:20:49 +0300

    change test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
+# Очищаем файл результатов перед началом
+> results.txt
+
 for input_formula in {1..4} # изменяем размер матрицы
 do
     for block_size in {1..10} # изменяем размер блока
     do
         for matrix_size in {10..20} # изменяем размер вывода
         do
-            ./a.out $matrix_size $block_size 0 $input_formula # запускаем программу с разными аргументами
+            echo "Running with matrix_size=$matrix_size, block_size=$block_size, input_formula=$input_formula" >> results.txt
+            ./a.out $matrix_size $block_size 0 $input_formula >> results.txt 2>&1
+            echo "----------------------------------------" >> results.txt
         done
     done
 done
